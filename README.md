91f11be04699fef1c556efe6173f5457608b1fca
Samuel ROZE <samuel.roze@gmail.com>
2013-06-04 14:39:53 +0200

    Create an empty view. Add browser router and start history Use LESS styles

--- README.md
+++ README.md
@@ -2,3 +2,12 @@ backbone-file-browser
 =====================
 
 A backbone file browser. (still in development)
+
+## Requirements
+
+- Backbone.js
+  - Underscore
+  - JSON2
+  - jQuery
+  
+- LESS
--- js/backbone-file-browser.js
+++ js/backbone-file-browser.js
@@ -6,11 +6,69 @@
 (function(factory){
 	// TODO AMD and require.js declarations
 	
-	Backbone.View.Browser = factory;
-}(function($, Backbone)){
-	var browser = Backbone.View.extend({
+	// Create the backbone view
+	Backbone.Browser = factory(jQuery, Backbone);
+	
+	// Create the jQuery extension
+	if (jQuery !== undefined) {
+		jQuery.browser = {
+	        instances : []
+	    };
 		
+		// Create the jQuery wrapper
+		var jQueryBrowser = function (element, options) {
+			// Create browser view
+			var browserView = new Backbone.Browser(options);
+			$(element).html(browserView.el);
+			
+			// Create browser router
+			var BrowserRouter = Backbone.Router.extend({
+			    routes:{
+			        "": "tree",
+			        "tree/*path": "tree"
+			    },
+			    
+			    tree: function (path) {
+			    	if (path == undefined) {
+			    		path = "root";
+			    	}
+			    	
+			    	browserView.browse(path);
+			    }
+			});
+			
+			// Start history
+			new BrowserRouter();
+			Backbone.history.start();
+		};
+	    
+		// Create function
+		jQuery.fn.browser = function (selectOptions) {
+	        return this.each(function () {
+	            var browser = new jQueryBrowser(this, selectOptions);
+	            jQuery.browser.instances.push(browser);
+	        });
+	    };
+	}
+}(function($, Backbone){
+	var baseView = Backbone.DeferedView.extend({
+		templateName: 'browser-view',
+		currentPath: 'root',
+		
+		render: function ()
+		{
+			this.$el.html(this.template({
+				path: this.currentPath
+			}));
+		},
+		
+		browse: function (path)
+		{
+			this.currentPath = path;
+			
+			this.deferedRender();
+		}
 	});
 	
-	return browser;
-});
+	return baseView;
+}));
