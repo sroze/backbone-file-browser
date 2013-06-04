9a84e353260dcbf6efcd5119609d7e3ea03e625c
Samuel ROZE <samuel.roze@gmail.com>
2013-06-04 11:39:13 +0200

    Create the JS file.

---
+++ js/backbone-file-browser.js
@@ -0,0 +1,16 @@
+/**
+ * Backbone.js File Browser
+ *
+ * @author Samuel ROZE
+ */
+(function(factory){
+	// TODO AMD and require.js declarations
+	
+	Backbone.View.Browser = factory;
+}(function($, Backbone)){
+	var browser = Backbone.View.extend({
+		
+	});
+	
+	return browser;
+});
