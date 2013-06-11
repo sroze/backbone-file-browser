3fd3e2568387d781097769a89b2ab9d6c889723f
Samuel ROZE <samuel.roze@gmail.com>
2013-06-11 16:00:28 +0200

    Add breadcrumb, update information view, allow inline edit of description

--- README.md
+++ README.md
@@ -5,9 +5,16 @@ A backbone file browser. (still in development)
 
 ## Requirements
 
+- jQuery
+  - jBreadCrumb
+    - easing
+
 - Backbone.js
   - Underscore
   - JSON2
-  - jQuery
-  
+
+- SuperModel.js
+- backbone-defered-view-loader
+- backbone-cache (@see https://github.com/naorye/BackboneCache)
 - LESS
+- http://vitalets.github.io/x-editable
--- js/backbone-file-browser.js
+++ js/backbone-file-browser.js
@@ -38,7 +38,7 @@
             });
             
             // Start history
-            new BrowserRouter();
+            browserView.setRouter(new BrowserRouter());
             Backbone.history.start();
         };
         
@@ -51,7 +51,32 @@
         };
     }
 }(function($, Backbone){
-    var userOptions = {
+    
+    /**
+     * Multiple extends.
+     * 
+     */
+    Backbone.Model.extendEach      =
+    Backbone.Collection.extendEach =
+    Backbone.Router.extendEach     =
+    Supermodel.Model.extendEach    =
+    Backbone.PageableCollection.extendEach =
+    Backbone.View.extendEach       = function () {
+        var args  = Array.prototype.slice.call(arguments),
+            child = this;
+     
+        _.each(args, function (proto) {
+          child = child.extend(proto);
+        });
+     
+        return child;
+    };
+    
+    /**
+     * Browser configuration.
+     * 
+     */
+    var router = null, currentFolder = null, userOptions = {
         file_url: '/file/__file_id__',
         children_url: '/children/__file_id__',
         
@@ -59,20 +84,30 @@
         sortColumns: {},
         displayedColumns: ["btn-checkbox", "icon", "name", "size", "actions"],
         showBar: true
-    };
+    }, browserCache = new Backbone.Cache();
     
     /**
      * A file metadata.
      * 
      */
-    var FileMetadata = Backbone.Model.extend({
-	getValue: function () {
-	    return this.get('value');
+    var FileMetadata = Supermodel.Model.extendEach(Backbone.CachedModel.prototype, {
+	cacheObject: browserCache,
+	
+	getName: function () {
+	    return this.get('name');
 	},
 	
-	getDescription: function () {
-	    return this.get('description');
-	}
+        getValue: function () {
+            return this.get('value');
+        },
+        
+        setValue: function (value) {
+            this.set('value', value);
+        },
+        
+        getDescription: function () {
+            return this.get('description');
+        }
     });
     
     /**
@@ -82,26 +117,75 @@
      * Attributes:
      * - id
      */
-    var File = Backbone.Model.extend({
-	TYPE_FILE: 1,
-	TYPE_FOLDER: 2,
+    var File = Supermodel.Model.extendEach(Backbone.CachedModel.prototype, {
+	cacheObject: browserCache,
 	
+        TYPE_FILE: 1,
+        TYPE_FOLDER: 2,
+        parents: null,
+        
         constructor: function (attributes, options) {
-            Backbone.Model.apply(this, arguments);
+            Supermodel.Model.apply(this, arguments);
             this.options = options;
+            this.cacheKey = 'file_'+this.get('id');
             
             var self = this;
             this.url = function () {
-                return userOptions.file_url.replace('__file_id__', self.model.get('id'));
+                return userOptions.file_url.replace('__file_id__', self.get('id'));
             }
+        },
+        
+        // Return file parents by calling the callback
+        // with array of parents collection
+        getParents: function (callback)
+        {
+            if (this.parents == null || true) {
+                var self = this;
+                console.log('getParents -> resursive');
+                this.recursiveGetParents(function(parents){
+                    self.parents = parents;
+                    callback(parents);
+                });
+            } else {
+                console.log('getParents cache');
+                return this.parents;
+            }
+        },
+        
+        // Get parents with recursive function
+        recursiveGetParents: function (callback)
+        {
+            if (this.get('parentId') != null) {
+                var parent = File.create({id: this.get('parentId')});
+                parent.fetch({cache: true, expires: 60}).done(function(file){
+                    parent.recursiveGetParents(function(parents){
+                        parents.push(parent);
+                        console.log('recursiveGetParents parent #'+parents.length);
+                        callback(parents);
+                    });
+                }).fail(function(){
+                    callback([]);
+                })
+            } else {
+                callback([]);
+            }
+        },
+        
+        // Get children collection, not fetched.
+        getChildren: function ()
+        {
+            return new FileCollection([], {
+                children_url: userOptions.children_url,
+                parentId: this.get('id')
+            })
         },
         
         // Parse the metadatas
         parse: function (resp, options) {
             if (resp.metadatas != undefined) {
-        	for (var key in resp.metadatas) {
-        	    resp.metadatas[key] = new FileMetadata(resp.metadatas[key], options);
-        	}
+                for (var key in resp.metadatas) {
+                    resp.metadatas[key] = FileMetadata.create(resp.metadatas[key], options);
+                }
             }
             
             return resp;
@@ -109,16 +193,26 @@
     
         // Return object that represents this model for browser
         getBrowserData: function () {
-            return $.extend({
-    	    	size: 'TODO'
-            }, this.toJSON());
+            return this.toJSON();
         },
         
         getMetadata: function (key) {
             var metadatas = this.get('metadatas');
             if (metadatas[key] != undefined) {
-        	return metadatas[key];
+                return metadatas[key];
             }
+        },
+        
+        setMetadata: function (key, value) {
+            var metadatas = this.get('metadatas');
+            if (metadatas[key] == undefined) {
+                metadatas[key] = new FileMetadata({
+                    name: key
+                });
+            }
+            
+            metadatas[key].setValue(value);
+            this.set('metadatas', metadatas);
         },
         
         isFile: function () {
@@ -136,9 +230,9 @@
      * - children_url
      * - parentIf
      */
-    var FileCollection = Backbone.PageableCollection.extend({
+    var FileCollection = Backbone.PageableCollection.extendEach(/*Backbone.CachedCollection,*/ {
         model: function (attributes, options) {
-            return new File(attributes, options);
+            return File.create(attributes, options);
         },
         
         constructor: function (models, options) {
@@ -148,7 +242,7 @@
             
             var self = this;
             this.url = function () {
-        	return userOptions.children_url.replace('__file_id__', self.options.parentId)
+                return userOptions.children_url.replace('__file_id__', self.options.parentId)
             }
         },
 
@@ -177,33 +271,83 @@
      * 
      */
     var InformationView = Backbone.DeferedView.extend({
-	tagName:"div",
+        tagName:"div",
         templateName: "browser-information",
         className: "browser-information span2",
         provider: null,
 
         initialize:function () {
-            this.model.bind("all", this.deferedRender, this);
-            this.model.bind("destroy", this.close, this);
-        },
-        
-        setProvider: function (provider) {
-            this.provider = provider;
+            this.model.on("all", this.deferedRender, this);
+            this.model.on("destroy", this.close, this);
         },
 
         render:function (eventName) {
             var view = this;
             $(this.el).html(this.template(this.computeData()));
+            
+            // Add events
+            var selected_model = this.model.length > 0 ? this.model.at(0) : currentFolder;
+            var editable_field = $('#file_description', this.el).editable({
+        	title: 'Enter description',
+        	placement: 'left'
+            }).on('save', function(e, params) {
+        	selected_model.setMetadata("description", params.newValue);
+        	selected_model.save();
+            });
+            
+            
+            $('i#edit_description', this.el).click(function(){
+        	editable_field.editable('show');
+        	return false;
+            })
         },
         
         computeData: function () {
             return _.extend({
                 selection: this.model,
-                provider: this.provider
+                current: currentFolder
             }, this.getHelpers());
         }
     });
     
+    /**
+     * The browser bar.
+     * 
+     */
+    var BrowserBarView = Backbone.DeferedView.extend({
+        templateName: "browser-bar",
+        tagName: "div",
+        className: "browser-bar",
+        
+        browse: function (file) {
+            this.file = file;
+        },
+        
+        render: function (eventName) {
+            $(this.el).html(this.template());
+
+            // Display breadcrumb
+            var bc = $('div.breadcrumb', self.el);
+            var file = this.file;
+            this.file.getParents(function(parents){
+                for (var i = 0; i < parents.length; i++) {
+                    var parent = parents[i];
+                    $('ul', bc).append('<li><a href="#tree/'+parent.get('id')+'">'+parent.get('name')+'</a></li>');
+                }
+                
+                // Append current directory
+                $('ul', bc).append('<li><a href="#tree/'+file.get('id')+'">'+file.get('name')+'</a></li>');
+                
+                // jBreadcrumb-ize
+                bc.jBreadCrumb();
+            });
+        }
+    });
+    
+    /**
+     * An item in the list view.
+     * 
+     */
     var ItemView = Backbone.DeferedView.extend({
 
         tagName:"tr",
@@ -211,11 +355,11 @@
         className: "item browser-item",
 
         initialize:function () {
-            this.model.bind("change", this.deferedRender, this);
-            this.model.bind("destroy", this.close, this);
+            this.model.on("change", this.deferedRender, this);
+            this.model.on("destroy", this.close, this);
             
             // Bind on columns change
-            this.options.listView.model.bind("columnsChange", this.refreshColumns, this);
+            this.options.listView.model.on("columnsChange", this.refreshColumns, this);
         },
         
         // Dispatch selection event to browser list view
@@ -249,13 +393,7 @@
             // Add line click event
             $('td', this.el).unbind('click').bind('click', function(e){
                 if ($(e.target).not('div.btn-checkbox, div.checkbox-inner, a, img#img-pie').length == 1) {
-                    if (!view.model.isFile()) {
-                        // Redirect user to the folder or provider browsing
-                        // Display informations and update URL
-                        lmc.app.navigate("browse/"+view.model.get("url_path"), {trigger: true});
-                    } else {
-                        $('div.btn-checkbox', view.el).click();
-                    }
+                    $('div.btn-checkbox', view.el).click();
                 }
             });
             
@@ -296,6 +434,20 @@
         informationView: null,
         synched: false,
         templateLoaded: false,
+        loadedCountDown: 2,
+        
+        // Unbind model events on close
+        onClose: function () {
+            this.model.off("reset", this.render, this);
+            this.model.off("columnsChange", this.columnsChanged, this);
+            this.model.off("selectionChange", this.selectionChanged, this);
+            this.model.off("add", this.addDataObject, this);
+            this.model.off("sync", undefined, this);
+            
+            if (this.informationView != null) {
+                this.informationView.close();
+            }
+        },
 
         initialize:function () {
             this.selected = new FileCollection([], {
@@ -304,25 +456,14 @@
             
             // Add models events
             var self = this;
-            this.model.bind("reset", this.render, this);
-            this.model.bind("columnsChange", this.columnsChanged, this);
-            this.model.bind("selectionChange", this.selectionChanged, this);
-            this.model.bind("add", this.addDataObject, this);
-            this.model.bind("sync", function () {
-        	this.synched = true;
-        	this.isLoaded(undefined);
+            this.model.on("reset", this.render, this);
+            this.model.on("columnsChange", this.columnsChanged, this);
+            this.model.on("selectionChange", this.selectionChanged, this);
+            this.model.on("add", this.addDataObject, this);
+            this.model.on("sync", function () {
+                this.synched = true;
+                this.isLoaded(true);
             }, this);
-
-            // Add informations views
-            // TODO this.breadcrumb = new BrowserBreadcrumbView({path: userOptions.path, userId: userOptions.userId, context: "folder"});
-        },
-        
-        isLoaded: function (loaded) {
-            if (loaded != undefined) {
-        	this.templateLoaded = loaded;
-            }
-            
-            Backbone.DeferedView.prototype.isLoaded.apply(this, [this.templateLoaded && this.synched]);
         },
         
         setSorting: function (key, sort) {
@@ -456,11 +597,8 @@
             $(this.el).empty();
             $(this.el).html(this.template(this.model.toJSON()));
             
-            // Display breadcrumb
-            // TODO $('div.browser-bar', this.el).append(this.breadcrumb.render().el);
-            
             // Add header events
-            var multiCheckbox = $('div#selector-toggle div.btn-checkbox', $(this.el).parent());
+            var multiCheckbox = $('div#selector-toggle div.btn-checkbox', this.el);
             multiCheckbox.unbind('click').bind('click', function(e){
                 self.multiSelectorClicked(this);
             });
@@ -485,8 +623,6 @@
                 $(this.el).append(this.informationView.deferedRender().el);
             }
             
-            // Refresh browser bar and its events
-            this.refreshBrowserBar();
             this.columnsChanged(userOptions.displayedColumns);
             
             return this;
@@ -562,22 +698,13 @@
             $('ul#dd-menu-columns input[type="checkbox"]', browserbar).unbind('click').bind('click', function (e){
                 self.setColumnSelected($(this).attr("value"), $(this).is(':checked'));
             });
-            
-            // Copy button
-            $('div.btn-group-file button#btn-copy').unbind('click').bind('click', function(e){
-                // Create file collection
-                var files = self.getSelected();
-                
-                // Start copy
-                self.copyOf(files);
-            });
         }
     });
     
     var BaseView = Backbone.DeferedView.extend({
-        templateName: 'browser-view',
         currentPath: 'root',
         tagName: 'div',
+        loadedCountDown: 0,
         
         initialize: function ()
         {
@@ -586,24 +713,56 @@
         
         render: function ()
         {
-            this.$el.html(this.template());
+            this.clear();
+            this.isLoaded(false);
+            
+            // Get file object
+            currentFolder = File.create({id: this.currentPath}), self = this;
+            
+            // Add browser bar
+            if (this.browserbar == null) {
+                this.browserbar = new BrowserBarView();
+            }
+            this.browserbar.browse(currentFolder);
+            
+            // Refresh browser bar and its events
+            $(this.el).prepend(this.browserbar.el);
             
             // Render list
-            var files = new FileCollection([], {
-                children_url: userOptions.children_url,
-                parentId: this.currentPath
+            var files = currentFolder.getChildren();
+            console.log('fetch file from BaseView:render');
+            currentFolder.fetch({cache: true, expires: 60}).done(function(){
+        	self.browserbar.deferedRender();
+        	
+                self.list = new ListItemView({model: files});
+                self.isLoaded(true);
+                
+                $(self.el).append(self.list.deferedRender().el);
+                
+                console.log('fetch children file from BaseView:render');
+                files.fetch({cache: true, expires: 60}).fail(function(){
+                    // FIXME
+                });
+            }).fail(function(){
+                // FIXME
             });
-            files.fetch();
-            
-            this.list = new ListItemView({model: files});
-            this.list.renderTo($('div#browser-list'));
+        },
+        
+        setRouter: function (instance_router) {
+            router = instance_router;
         },
         
         browse: function (path)
         {
             this.currentPath = path;
-            
-            this.deferedRender();
+
+            this.render();
+        },
+        
+        clear: function () {
+            if (this.list != null) {
+                this.list.close();
+            }
         }
     });
     
